80639dfbfab573857f364db830f27f2e3e9583ee
OldCodeFans <songjiangzhang@hotmail.com>
2023-01-03 17:08:02 +0800

    update fib

--- algorithm_in_real_question/fib_question.cpp
+++ algorithm_in_real_question/fib_question.cpp
@@ -19,12 +19,13 @@ int fib(int n)
 
 int fib_dp(int n)
 {
+    vector<int> dp(n+1, 0);
+
     if (n == 0)
         return 0;
     if (n == 1 || n == 2)
         return 1;
 
-    vector<int> dp(n+1, 0);
 
     dp[1] = dp[2] = 1;
 
@@ -40,12 +41,12 @@ int main(int argc, char const *argv[])
     clock_t start, end;
     start = clock();
     cout << fib(3) << endl;
-    cout << fib(30) << endl;
+    cout << fib(5) << endl;
     end = clock();
     cout << "time = " << double(end - start) / 1000 << "s" << endl; // 输出时间（单位：ｓ）
     start = clock();
     cout << fib_dp(3) << endl;
-    cout << fib_dp(45) << endl;
+    cout << fib_dp(6) << endl;
     end = clock();
     cout << "time = " << double(end - start) / 1000 << "s" << endl; // 输出时间（单位：ｓ）
 
