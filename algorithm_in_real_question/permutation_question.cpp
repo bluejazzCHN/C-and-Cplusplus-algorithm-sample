eb1d8597f7b25f9a090a34920d39a2875c950924
OldCodeFans <songjiangzhang@hotmail.com>
2023-01-04 08:38:58 +0800

    add comment in permutation demo

--- algorithm_in_real_question/permutation_question.cpp
+++ algorithm_in_real_question/permutation_question.cpp
@@ -4,12 +4,15 @@
 
 using namespace std;
 
-vector<vector<int>> res; // 保存排序结果
-vector<int> track;       // 回溯路径，完整的track结果会保存在res中。完整track条件为track的size等于nums的size。
+vector<vector<int>> res; // 全排序的结果集
+vector<int> track;       // 一条回溯路径回溯路径
 
+// 路径：记录在track对象中
+// 路径元素选择列表：nums中不在track中的
+// 结束条件：nums的元素全部在track中存在
 void backtrack(vector<int> nums, vector<int> track)
 {
-    // 判断track是否已经完成遍历
+    // 出发结束条件，结束条件：判断track是否已经完成遍历
     if (track.size() == nums.size())
     {
         res.push_back(track);
@@ -23,14 +26,19 @@ void backtrack(vector<int> nums, vector<int> track)
         {
             continue;
         }
+
+        // 做选择，将n保存在track中
         track.push_back(n); // 加入track
+        // 进入下一层决策
         backtrack(nums, track);
+        // 取消选择，将n移除从track中
         track.pop_back(); // 清空track
     }
 }
 int main(int argc, char const *argv[])
 {
-    vector<int> a = {1, 2, 3, 6};
+    vector<int> a = {6, 2, 3, 1, 0};
+    sort(a.begin(), a.end(), greater());
 
     // 回溯，得出a的全排列结果保存在res中。
     backtrack(a, track);
